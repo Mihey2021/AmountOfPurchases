28c429220d73f3b1d37249fd524c4e02b388e2a2
Mihey2021 <hunter_m@list.ru>
2022-03-26 13:59:27 +0300

    Переименован MELOMAN_DISCOUNT -> MELOMAN_DISCOUNT_PERCENT

--- src/main/kotlin/AmountOfPurchases.kt
+++ src/main/kotlin/AmountOfPurchases.kt
@@ -2,7 +2,7 @@ import kotlin.random.Random
 
 const val STANDARD_DISCOUNT_RUB = 100.0
 const val DISCOUNT_PERCENT = 0.05
-const val MELOMAN_DISCOUNT = 0.01
+const val MELOMAN_DISCOUNT_PERCENT = 0.01
 
 fun main() {
     print("Введите сумму покпок (руб.): ")
@@ -11,7 +11,7 @@ fun main() {
     val discountSum =
         if (amountOfPurchases > 10000) amountOfPurchases * DISCOUNT_PERCENT else if (amountOfPurchases > 1000) STANDARD_DISCOUNT_RUB else 0.0
     val sum = amountOfPurchases - discountSum
-    val melomanDiscountSum = if (userIsMeloman) sum * MELOMAN_DISCOUNT else 0.0
+    val melomanDiscountSum = if (userIsMeloman) sum * MELOMAN_DISCOUNT_PERCENT else 0.0
     val totalSum = sum - melomanDiscountSum
 
     println("Пользователь меломан: $userIsMeloman. \nСкидка от суммы покупки: $discountSum руб. Скидка меломану: $melomanDiscountSum руб. \nИтоговая сумма: $totalSum руб.")
